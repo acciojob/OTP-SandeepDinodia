0e309f91a073566c465bf4d4dad17a91669dab70
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-08-16 15:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ function onCodeInput(index) {
+            if (index < 6) {
+                document.querySelector(`.code:nth-child(${index + 1})`).value = event.target.value;
+                document.querySelector(`.code:nth-child(${index + 2})`).focus();
+            } else {
+                document.querySelector(`.code:nth-child(${index})`).value = event.target.value;
+            }
+        }
+
+        function onBackspace(index) {
+            if (event.key === 'Backspace' && index > 1) {
+                document.querySelector(`.code:nth-child(${index - 1})`).focus();
+            }
+        }
