b9b8bd15de5b707e6a611d2baddab3a72c49a832
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-08-16 16:29:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function onCodeInput(index){
-	if(index < 6){
-document.getElementsByClassName('code')[index].focus();
+	if(index < 5){
+document.getElementsByClassName('code')[index+1].focus();
 	}
 }
 function onBackspace(index,event){
-	if(event.keyCode ===8 && index > 1){
-document.getElementsByClassName('code')[index-2].focus();
+	if(event.keyCode ===8 && index > 0){
+document.getElementsByClassName('code')[index-1].focus();
 	}
 }
